99479fb26a618f821f6d2a4a298b76ed9b0707f3
Kevin <info@technzoer.com>
2021-03-05 17:56:31 +0530

    initial commit

---
+++ AccuraOCR.framework/Headers/AccuraOCR.h
@@ -0,0 +1,28 @@
+//
+//  AccuraOCR.h
+//  AccuraOCR
+//
+//  Created by Technozer on 17/05/20.
+//  Copyright © 2020 Technozer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AccuraOCR.
+FOUNDATION_EXPORT double AccuraOCRVersionNumber;
+
+//! Project version string for AccuraOCR.
+FOUNDATION_EXPORT const unsigned char AccuraOCRVersionString[];
+
+#import <AccuraOCR/GlobalMethods.h>
+#import <AccuraOCR/VideoCameraWrapperDelegate.h>
+#import <AccuraOCR/AccuraCameraWrapper.h>
+#import <AccuraOCR/SDKModels.h>
+#import <AccuraOCR/LivenessData.h>
+#import <AccuraOCR/NSObject+Liveness.h>
+#import <AccuraOCR/NSObject+Constant.h>
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccuraOCR/PublicHeader.h>
+
+
---
+++ AccuraOCR.framework/Headers/LivenessVC.h
@@ -0,0 +1,42 @@
+//
+//  LivenessVC.h
+//  AccuraOCR
+//
+//  Created by Technozer on 17/02/21.
+//  Copyright © 2021 Technozer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+#import "AccuraCameraWrapper.h"
+#import "UIView+ViewExtension.h"
+#import "UIView+customView.h"
+#import "NSObject+userDefaults.h"
+#import "LivenessData.h"
+#import "NSObject+PostResult.h"
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LivenessVC : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIView *viewLiveness;
+@property (weak, nonatomic) IBOutlet UIButton *buttonBack;
+@property (weak, nonatomic) IBOutlet UILabel *lblOCRMsg;
+@property (weak, nonatomic) IBOutlet UIView *viewSetLOGO;
+@property (weak, nonatomic) IBOutlet UIView *viewLogo;
+@property (weak, nonatomic) IBOutlet UIView *viewTitleLable;
+@property (weak, nonatomic) IBOutlet UILabel *labelTitle;
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *_constant_height;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *_constant_width;
+
+@property (nonatomic, strong) id<LivenessData> delegate;
+@property (nonatomic, assign) bool ischeckLiveness;
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
